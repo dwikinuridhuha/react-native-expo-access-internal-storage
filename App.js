d66f28a1154a4a2683b662df2de224161d0c25e1
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2022-01-22 18:24:39 +0700

    just for fun

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import * as MediaPicker from "expo-image-picker";
 
 export default function App() {
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    _accessStatusGalleryPermission();
+  }, []);
+
+  async function _accessStatusGalleryPermission() {
+    const galleryStatus =
+      await MediaPicker.requestMediaLibraryPermissionsAsync();
+
+    setHasGalleryPermission(galleryStatus.status === "granted");
+  }
+
+  async function _pickImage() {
+    let resultImagePicker = await MediaPicker.launchImageLibraryAsync({
+      mediaTypes: MediaPicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(resultImagePicker);
+
+    if (!resultImagePicker.cancelled) {
+      setImage(resultImagePicker.uri);
+    }
+
+    if (hasGalleryPermission === false) {
+      return <Text>No access to open image</Text>;
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Pick Image From Gallery"
+        onPress={() => {
+          _pickImage();
+        }}
+      />
+
+      {image && <Image source={{ uri: image }} style={{ flex: 1 / 2 }} />}
     </View>
   );
 }
@@ -13,8 +53,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# React Native with expo access internal storage
+
+just for fun exploring react native.
